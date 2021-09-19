64d766c783f5c2b282c4a45cc16973e19378a148
Andrés Loreto <andresantigoloretoaguero@gmail.com>
2021-09-18 22:59:27 -0400

    Started to work on authentication

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ var promoRouter = require('./routes/promoRouter');
 
 const mongoose = require('mongoose');
 
-const Dishes = require('./models/dishes');
-
 const url = require('./config');
 const connect = mongoose.connect(url);
 
@@ -33,6 +31,39 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const auth = (req, res, next) => {
+	console.log(req.headers);
+
+	var authHeader = req.headers.authorization;
+
+	if (!authHeader) {
+		var err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		next(err);
+		return;
+	}
+
+	var auth = new Buffer(authHeader.split(' ')[1], 'base64')
+		.toString()
+		.split(':');
+
+	var username = auth[0];
+	var password = auth[1];
+
+	if (username === 'admin' && password === 'password') {
+		next();
+	} else {
+		var err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		next(err);
+	}
+};
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
