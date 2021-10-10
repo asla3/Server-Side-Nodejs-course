63b68f62bafe27216cdca53902d75df938f493ba
Andrés Loreto <andresantigoloretoaguero@gmail.com>
2021-10-10 15:05:37 -0400

    Set up https for the server

--- app.js
+++ app.js
@@ -28,6 +28,17 @@ connect
 	.catch((err) => console.log(err));
 
 var app = express();
+// redirect insecure requests to https
+app.all('*', (req, res, next) => {
+	if (req.secure) {
+		return next();
+	} else {
+		res.redirect(
+			307,
+			'https://' + req.hostname + ':' + app.get('secPort') + req.url
+		);
+	}
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
